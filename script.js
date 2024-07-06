c8e58719b8ef4542813d0a982f95e8bbdcdd8128
POTHULA. JAI SAI <92910772+jaisaipothula@users.noreply.github.com>
2024-07-06 21:23:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 function firstWord(s) {
   // your code here
+	function firstWord(str: string): string {
+    // Handle edge case: empty string
+    if (str.length === 0) {
+        return "";
+    }
+
+    // Find the index of the first space
+    const spaceIndex = str.indexOf(' ');
+
+    // If spaceIndex is -1, return the entire string
+    if (spaceIndex === -1) {
+        return str;
+    }
+
+    // Otherwise, return substring from start to spaceIndex
+    return str.substring(0, spaceIndex);
+}
+
 }
 
 // Do not change the code below
